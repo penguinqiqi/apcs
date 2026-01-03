9a8339e09a4d59aab59aea0afec1c6841ad87fd9
penguinqiqi <dolphinqiqi9.3@gmail.com>
2026-01-03 19:58:31 +0800

    商品包裝地

--- r490.3_Product packaging place.py
+++ r490.3_Product packaging place.py
@@ -14,9 +14,9 @@ for i in range(a):
         
     if (Sodd+3*Seven)%10+product[12]==0 or (Sodd+3*Seven)%10+product[12]==10:
         place.append(product_char[0:3])
-
-most=Counter(place).most_common(1)[0][0]
-ans=place.count(most)
+                                         #Counter(place)將列表轉換為一個計數字典:Counter({'471': 2, '030': 1, '300': 1})
+most=Counter(place).most_common(1)[0][0] #.most_common(1)統計結果中，找出出現次數前 1 名的資料
+ans=place.count(most)                    #[0] (第一個索引)列表中取出第一個元素('471', 2) [0] (第二個索引)從元組 ('471', 2) 中取出索引為 0 的值。
 print(f"%s %d"%(most,ans))
 
 
